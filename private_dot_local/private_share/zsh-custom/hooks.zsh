5ebdff94ecdc19404cf83ff62c0df53219ee3067
erdii <me@erdii.engineering>
2023-09-05 15:12:40 +0200

    print to term instead of sending notification for long running commands

--- private_dot_local/private_share/zsh-custom/hooks.zsh
+++ private_dot_local/private_share/zsh-custom/hooks.zsh
@@ -11,8 +11,8 @@ function precmd() {
   if [ $TIMER ]; then
     local elapsed_seconds=$(($SECONDS - $TIMER))
     unset TIMER
-    if [ "$elapsed_seconds" -gt 60 ]; then
-      notify-send "[term] ${LASTHIST} took ${elapsed_seconds}s to run"
+    if [ "$elapsed_seconds" -gt 1 ]; then
+      echo "${=LASTHIST}\ntook ${elapsed_seconds}s to run"
     fi
   fi
 }
